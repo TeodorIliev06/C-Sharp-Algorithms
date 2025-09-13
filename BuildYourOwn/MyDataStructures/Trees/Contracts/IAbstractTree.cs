046e3288973b8b29f5445122c8b8c344d66d2a87
Bozhidara Partenova <141734012+TeodorIliev06@users.noreply.github.com>
2025-09-13 21:23:55 +0300

    feat: initialize tree traversal methods

---
+++ BuildYourOwn/MyDataStructures/Trees/Contracts/IAbstractTree.cs
@@ -0,0 +1,19 @@
+﻿namespace MyDataStructures.Trees.Contracts
+{
+    using System.Collections.Generic;
+
+    using MyDataStructures.Trees.Models;
+
+    interface IAbstractTree<T>
+    {
+        IEnumerable<T> OrderBfs();
+
+        IEnumerable<T> OrderDfs();
+
+        void AddChild(T parentKey, Tree<T> child);
+
+        void RemoveNode(T nodeKey);
+
+        void Swap(T firstKey, T secondKey);
+    }
+}
---
+++ BuildYourOwn/MyDataStructures/Trees/Models/Tree.cs
@@ -0,0 +1,157 @@
+﻿namespace MyDataStructures.Trees.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MyDataStructures.Trees.Contracts;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private T value;
+        private Tree<T> parent;
+        private List<Tree<T>> children;
+
+        public Tree(T value)
+        {
+            this.value = value;
+            this.children = new List<Tree<T>>();
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            foreach (var child in children)
+            {
+                child.parent = this;
+                this.children.Add(child);
+            }
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            var parentTree = this.FindNodeWithBfs(parentKey);
+            this.CheckIfEmpty(parentTree);
+
+            parentTree.children.Add(child);
+            child.parent = parentTree;
+        }
+
+        public IEnumerable<T> OrderBfs()
+        {
+            var result = new List<T>();
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0) 
+            {
+                var subtree = queue.Dequeue();
+                result.Add(subtree.value);
+
+                foreach (var child in subtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerable<T> OrderDfs()
+        {
+            var result = new List<T>();
+            this.Dfs(this, result);
+
+            return result;
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            var node = this.FindNodeWithBfs(nodeKey);
+            this.CheckIfEmpty(node);
+
+            var parentNode = node.parent;
+            this.CheckIfRoot(parentNode);
+
+            // Remove directly the subtree and its descendents
+            parentNode.children.Remove(node);
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            var firstNode = this.FindNodeWithBfs(firstKey);
+            this.CheckIfEmpty(firstNode);
+
+            var secondNode = this.FindNodeWithBfs(secondKey);
+            this.CheckIfEmpty(secondNode);
+
+            var firstParentNode = firstNode.parent;
+            this.CheckIfRoot(firstParentNode);
+
+            var secondParentNode = secondNode.parent;
+            this.CheckIfRoot(secondParentNode);
+
+            // 3 cases:
+            // 1. Swap two internal nodes
+            // 2. Swap two leaves
+            // 3. Swap internal with a leaf (leaf stays)
+
+            var firstNodeIndex = firstParentNode.children.IndexOf(firstNode);
+            var secondNodeIndex = secondParentNode.children.IndexOf(secondNode);
+
+            firstParentNode.children[firstNodeIndex] = secondNode;
+            secondNode.parent = firstParentNode;
+
+            secondParentNode.children[secondNodeIndex] = firstNode;
+            firstNode.parent = secondParentNode;
+        }
+
+        private void Dfs(Tree<T> node, List<T> result)
+        {
+            foreach (var child in node.children)
+            {
+                this.Dfs(child, result);
+            }
+
+            result.Add(node.value);
+        }
+
+        private Tree<T> FindNodeWithBfs(T key)
+        {
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var subtree = queue.Dequeue();
+
+                if (subtree.value.Equals(key))
+                {
+                    return subtree;
+                }
+
+                foreach (var child in subtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        private void CheckIfEmpty(Tree<T> node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        private void CheckIfRoot(Tree<T> parentNode)
+        {
+            if (parentNode == null)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
