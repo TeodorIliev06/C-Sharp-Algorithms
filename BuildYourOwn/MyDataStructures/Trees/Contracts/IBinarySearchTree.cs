1d37973dc4a8c006a4bfeb3bb462835dbbf9b92e
TeodorIliev06 <tedo2505@gmail.com>
2025-09-22 18:33:08 +0300

    feat: implement BST traversal operations
    - optimise to be efficient in O(log n)

--- BuildYourOwn/MyDataStructures/Trees/Contracts/IBinarySearchTree.cs
+++ BuildYourOwn/MyDataStructures/Trees/Contracts/IBinarySearchTree.cs
@@ -4,6 +4,7 @@
 
     public interface IBinarySearchTree<T> where T : IComparable<T>
     {
+        // Lab
         void Insert(T element);
 
         bool Contains(T element);
@@ -11,5 +12,24 @@
         void EachInOrder(Action<T> action);
 
         IBinarySearchTree<T> Search(T element);
+
+        // Exercise
+        void Delete(T element);
+
+        void DeleteMin();
+
+        void DeleteMax();
+
+        int Count();
+
+        int Rank(T element);
+
+        T Select(int rank);
+
+        T Ceiling(T element);
+
+        T Floor(T element);
+
+        IEnumerable<T> Range(T startRange, T endRange);
     }
 }
--- BuildYourOwn/MyDataStructures/Trees/Models/BinarySearchTree.cs
+++ BuildYourOwn/MyDataStructures/Trees/Models/BinarySearchTree.cs
@@ -18,6 +18,8 @@
 
             public Node Left { get; set; }
             public Node Right { get; set; }
+
+            public int Count { get; set; }
         }
 
         // Creating a new tree means we have to copy the nodes positioning
@@ -32,6 +34,8 @@
         }
 
         private Node root;
+
+        // Lab
         public bool Contains(T element)
         {
             return this.FindNode(element) != null;
@@ -57,6 +61,67 @@
             }
 
             return new BinarySearchTree<T>(node);
+        }
+
+        // Exercise
+        public void Delete(T element)
+        {
+            CheckIfNull(this.root);
+
+            this.root = this.Delete(this.root, element);
+        }
+
+        public void DeleteMin()
+        {
+            CheckIfNull(this.root);
+
+            this.root = this.DeleteMin(this.root);
+        }
+
+        public void DeleteMax()
+        {
+            CheckIfNull(this.root);
+
+            this.root = this.DeleteMax(this.root);
+        }
+
+        public int Count()
+        {
+            return this.Count(this.root);
+        }
+
+        public int Rank(T element)
+        {
+            return this.Rank(this.root, element);
+        }
+
+        public T Select(int rank)
+        {
+            CheckIfNull(this.root);
+
+            Node node = this.Select(this.root, rank);
+            CheckIfNull(node);
+
+            return node.Value;
+        }
+
+        public T Ceiling(T element)
+        {
+            return this.Select(this.Rank(element) + 1);
+        }
+
+        public T Floor(T element)
+        {
+            return this.Select(this.Rank(element) - 1);
+        }
+
+        public IEnumerable<T> Range(T startRange, T endRange)
+        {
+            var queue = new Queue<T>();
+
+            this.Range(this.root, startRange, endRange, queue);
+
+            return queue;
         }
 
         private void PreOrderCopy(Node node)
@@ -89,6 +154,8 @@
             {
                 node.Right = this.Insert(element, node.Right);
             }
+
+            node.Count = 1 + this.Count(node.Left) + this.Count(node.Right);
 
             return node;
         }
@@ -127,6 +194,182 @@
                 }
             }
             return node;
+        }
+
+        private Node Delete(Node node, T element)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            var comparisonValue = element.CompareTo(node.Value);
+
+            if (comparisonValue < 0)
+            {
+                node.Left = this.Delete(node.Left, element);
+            }
+            else if (comparisonValue > 0)
+            {
+                node.Right = this.Delete(node.Right, element);
+            }
+
+            /*
+                Found the element:
+                1. No children → remove the node (return null).
+                2. One child → promote that child.
+                3. Two children:
+                    Keep tree shape, replace the node’s value with 3.1 or 3.2:
+                        3.1 the smallest value in the right subtree (in-order successor)
+                        3.2 the largest value in the left subtree (in-order predecessor)
+                    Then, recursively delete that successor/predecessor node.
+            */
+            else
+            {
+                if (node.Left == null)
+                {
+                    return node.Right;
+                }
+
+                if (node.Right == null)
+                {
+                    return node.Left;
+                }
+
+                var descendant = this.FindMin(node.Right);
+
+                node.Value = descendant.Value;
+
+                node.Right = this.Delete(node.Right, descendant.Value);
+            }
+
+            node.Count = 1 + this.Count(node.Left) + this.Count(node.Right);
+
+            return node;
+        }
+
+        private Node FindMin(Node node)
+        {
+            if (node.Left == null)
+            {
+                return node;
+            }
+
+            return this.FindMin(node.Left);
+        }
+
+        private Node DeleteMin(Node node)
+        {
+            // Leftmost element does not have a left child
+            // Promote right child (if null -> set null)
+            if (node.Left == null)
+            {
+                return node.Right;
+            }
+
+            node.Left = this.DeleteMin(node.Left);
+            node.Count = 1 + this.Count(node.Left) + this.Count(node.Right);
+
+            return node;
+        }
+
+        private Node DeleteMax(Node node)
+        {
+            // Reversed logic of deleting the min element
+            if (node.Right == null)
+            {
+                return node.Left;
+            }
+
+            node.Right = this.DeleteMax(node.Right);
+            node.Count = 1 + this.Count(node.Left) + this.Count(node.Right);
+
+            return node;
+        }
+
+        private int Count(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return node.Count;
+        }
+
+        private int Rank(Node node, T element)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            if (element.CompareTo(node.Value) < 0)
+            {
+                return this.Rank(node.Left, element);
+            }
+
+            // Traversing right means we add current node and the sum of the left subtree.
+            if (element.CompareTo(node.Value) > 0)
+            {
+                return 1 + this.Count(node.Left) + this.Rank(node.Right, element);
+            }
+
+            return this.Count(node.Left);
+        }
+
+        private Node Select(Node node, int rank)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            int leftCount = this.Count(node.Left);
+
+            if (leftCount == rank)
+            {
+                return node;
+            }
+
+            if (leftCount > rank)
+            {
+                return this.Select(node.Left, rank);
+            }
+
+            return this.Select(node.Right, rank - (leftCount + 1));
+        }
+
+        private void Range(Node node, T startRange, T endRange, Queue<T> queue)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (startRange.CompareTo(node.Value) < 0)
+            {
+                this.Range(node.Left, startRange, endRange, queue);
+            }
+
+            if (startRange.CompareTo(node.Value) <= 0 &&
+                endRange.CompareTo(node.Value) >= 0)
+            {
+                queue.Enqueue(node.Value);
+            }
+
+            if (endRange.CompareTo(node.Value) > 0)
+            {
+                this.Range(node.Right, startRange, endRange, queue);
+            }
+        }
+
+        private static void CheckIfNull(Node node)
+        {
+            if (node == null)
+            {
+                throw new InvalidOperationException();
+            }
         }
     }
 }
