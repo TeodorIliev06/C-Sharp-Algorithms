8655a4b1c2a346fe64204faf0d5c2428c1f1ce5b
TeodorIliev06 <tedo2505@gmail.com>
2025-09-24 22:16:39 +0300

    test: update BST test cases for all newly implemented operations

--- MyUnitTests/Trees/BinaryTreeTests.cs
+++ MyUnitTests/Trees/BinaryTreeTests.cs
@@ -12,6 +12,7 @@ namespace MyUnitTests.Trees
     public class BinaryTreeTests
     {
         private IAbstractBinaryTree<int> tree;
+        private IAbstractBinaryTree<int> lcaTree;
 
         [SetUp]
         public void InitializeBinaryTree()
@@ -28,6 +29,21 @@ namespace MyUnitTests.Trees
                                 new BinaryTree<int>(20, null, null),
                                 new BinaryTree<int>(31, null, null)
                             )
+            );
+
+            this.lcaTree = new BinaryTree<int>(
+                12,
+                new BinaryTree<int>(5,
+                    new BinaryTree<int>(1, null, null),
+                    new BinaryTree<int>(8, null, null)
+                ),
+                new BinaryTree<int>(19,
+                    new BinaryTree<int>(16, null, null),
+                    new BinaryTree<int>(23,
+                        new BinaryTree<int>(21, null, null),
+                        new BinaryTree<int>(30, null, null)
+                    )
+                )
             );
         }
 
@@ -93,6 +109,98 @@ namespace MyUnitTests.Trees
             string actual = builder.ToString();
 
             Assert.AreEqual(expected, actual.Substring(0, actual.LastIndexOf(", ")));
+        }
+
+        [Test]
+        [TestCase(19, 16, 21)]
+        [TestCase(12, 1, 21)]
+        [TestCase(23, 21, 30)]
+        [TestCase(23, 23, 30)]
+        [TestCase(12, 23, 8)]
+        [TestCase(19, 19, 23)]
+        public void FindLowestCommonAncestor_WithExistingNodes_ReturnsCorrectAncestor(int expected, int firstNode, int secondNode)
+        {
+            Assert.AreEqual(expected, this.lcaTree.FindLowestCommonAncestor(firstNode, secondNode));
+        }
+
+        [Test]
+        [TestCase(5, 54)]
+        [TestCase(54, 5)]
+        [TestCase(51, 54)]
+        public void FindLowestCommonAncestor_WithInvalidNode_ThrowsException(int firstNode, int secondNode)
+        {
+            Assert.Throws<InvalidOperationException>(() => this.lcaTree.FindLowestCommonAncestor(firstNode, secondNode));
+        }
+
+        [Test]
+        public void TestFirstTopView()
+        {
+            var binaryTree =
+                new BinaryTree<int>(1,
+                        new BinaryTree<int>(2,
+                                new BinaryTree<int>(4, null, null),
+                                new BinaryTree<int>(5, null, null)),
+                        new BinaryTree<int>(3,
+                                new BinaryTree<int>(6, null, null),
+                                new BinaryTree<int>(7, null, null)));
+
+            var topViewActual = binaryTree.TopView()
+                .OrderBy(n => n)
+                .ToArray();
+
+            int[] topViewExpected = { 1, 2, 3, 4, 7 };
+
+            Assert.AreEqual(topViewExpected.Length, topViewActual.Length);
+            for (int i = 0; i < topViewExpected.Length; i++)
+            {
+                Assert.AreEqual(topViewExpected[i], topViewActual[i]);
+            }
+        }
+
+        [Test]
+        public void TestSecondTopView()
+        {
+            var binaryTree =
+              new BinaryTree<int>(1,
+                      new BinaryTree<int>(2,
+                              new BinaryTree<int>(4, null, null),
+                              new BinaryTree<int>(5, null, null)),
+                      new BinaryTree<int>(3, null, null));
+
+            var topViewActual = binaryTree.TopView()
+                .OrderBy(n => n)
+                .ToArray();
+
+            int[] topViewExpected = { 1, 2, 3, 4 };
+
+            Assert.AreEqual(topViewExpected.Length, topViewActual.Length);
+            for (int i = 0; i < topViewExpected.Length; i++)
+            {
+                Assert.AreEqual(topViewExpected[i], topViewActual[i]);
+            }
+        }
+
+        [Test]
+        public void TestThirdTopView()
+        {
+            var binaryTree =
+              new BinaryTree<int>(1,
+                      new BinaryTree<int>(2, null, null),
+                      new BinaryTree<int>(3,
+                                new BinaryTree<int>(6, null, null),
+                                new BinaryTree<int>(7, null, null)));
+
+            var topViewActual = binaryTree.TopView()
+                .OrderBy(n => n)
+                .ToArray();
+
+            int[] topViewExpected = { 1, 2, 3, 7 };
+
+            Assert.AreEqual(topViewExpected.Length, topViewActual.Length);
+            for (int i = 0; i < topViewExpected.Length; i++)
+            {
+                Assert.AreEqual(topViewExpected[i], topViewActual[i]);
+            }
         }
     }
 }
